9bd1f521417599dc2d519ec75dca9a4df98b5173
Your Name <you@example.com>
2019-02-09 10:30:14 +0100

    Update files marcONE project

--- code/app.js
+++ code/app.js
@@ -1,1 +1,17 @@
-alert("Benvenuti nel nostro bar");
+var app = {
+    
+    init :function(){
+    //alert("Benvenuti nel nostro sito");
+    $.get("https://api.github.com/users/wbigger")
+    .done(app.onSuccess);
+    },
+    onSuccess: function(data){
+        console.log(data);
+    }
+
+};
+
+var init = function(){
+  alert("Benvenuti nel nostro sito");
+}
+$(document).ready(app.init);
