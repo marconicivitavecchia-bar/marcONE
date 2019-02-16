f7644f96887fcc37e19c666bcd2f74cef17dd379
wbigger <wbigger@gmail.com>
2019-02-16 07:49:10 +0100

    get local json with user list

--- code/app.js
+++ code/app.js
@@ -1,17 +1,18 @@
 var app = {
-    
-    init :function(){
+
+  init: function () {
     //alert("Benvenuti nel nostro sito");
-    $.get("https://api.github.com/users/wbigger")
-    .done(app.onSuccess);
-    },
-    onSuccess: function(data){
-        console.log(data);
-    }
+    //$.get("https://api.github.com/users/wbigger")
+    $.get("api/data.json")
+      .done(app.onSuccess);
+  },
+  onSuccess: function (data) {
+    console.log(data);
+  }
 
 };
 
-var init = function(){
+var init = function () {
   alert("Benvenuti nel nostro sito");
 }
 $(document).ready(app.init);
